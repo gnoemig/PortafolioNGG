d39664f7d4de159ccd1d3537b1fce2891838e6f6
gnoemig <guzman.garcia.noemi@gmail.com>
2024-08-08 17:11:08 -0400

    Created project for job

--- src/component/common/index.tsx
+++ src/component/common/index.tsx
@@ -5,7 +5,7 @@ const Mainlayout = () => {
   return (
     <div className= " w-full flex flex-col gap-5">
       <h2 className="text-center font-bold md:text-3xl">Portafolio Noemi Guzman</h2>
-      <div className="lg:w-1/4 w-full h-[100vh] hidden md:block">
+      <div className="lg:w-1/4 w-full h-[100vh] hidden sm:block">
         <Navbar />
       </div>
       <div className=" lg:w-3/4 w-full">
