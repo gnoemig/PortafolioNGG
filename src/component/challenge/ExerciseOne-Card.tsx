51eb66425960808cdb35fc1efd1489b011442e83
gnoemig <guzman.garcia.noemi@gmail.com>
2024-08-15 17:04:17 -0400

    wwhite space

--- src/component/challenge/ExerciseOne-Card.tsx
+++ src/component/challenge/ExerciseOne-Card.tsx
@@ -4,8 +4,8 @@ import person1 from "../../assets/images/user-1.png"
 
 const ExerciseOneCard = () => {
   return (
-    <div className="flex flex-col justify-center content-center w-full rounded-lg border">
-      <div className="w-[500px] h-[200px] bg-[#973150]">
+    <div className="flex flex-col justify-center content-center w-full  md:w-[500px] rounded-lg border">
+      <div className="w-full h-[100px] bg-[#973150] rounded-lg">
          <div className="rounded-3xl w-[5vw]  h-[5vh]">
              <img src={person1} alt="img of contact" />
          </div>
